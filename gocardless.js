0b968f261b15ad308afe7b2303db1363b809e999
CSLTESTUSER <rajarajan.b@saksoft.co.in>
2016-05-19 17:55:43 +0530

    Update gocardless.js

--- gocardless.js
+++ gocardless.js
@@ -1,36 +1,41 @@
 
 
+
+
 $(function(){
+
 	$('#setupmandate').on('click',function(){
-			setupRedirectWorkflow();
+		setupRedirectWorkflow();
+
 	});
 });
+
 
 function setupRedirectWorkflow()
 {
 	$.ajax({ 
-		type:'POST',
+		method:'POST',
 		url:'https://api-sandbox.gocardless.com/redirect_flows',
-		beforesend:function(xhr){
-			xhr.setRequestHeader('GoCardless-Version','2015-07-06');
-			xhr.setRequestHeader('Authorization','Bearer U15KDpAy1-TrD7ebm4h8oe5LaCx_f8HdAoQbSmC8');
-		},
-		contentType:'application/json',
 		data: {  
-			"redirect_flows": {
-    		"description": "Security Device Purchase",
-    		"session_token": "SESS_somerandomalphanumerictoverify",
-    		"success_redirect_url": "http://csltestuser.github.io"
+			redirect_flows: {
+    		'description': 'Security Device Purchase',
+    		'session_token': 'SESS_somerandomalphanumerictoverify',
+    		'success_redirect_url': 'http://csltestuser.github.io'
   			}
   		},
-  		sucess:function(response){
-  			var data=$.parseJSON(response);
-  			console.log(data);
+  		headers : { 
+			'Access-Control-Allow-Headers' : 'Content-Type,GoCardless-Version,Authorization',
+			'Content-Type' :'application/json',
+			'GoCardless-Version':'2015-07-06',
+			'Authorization':'Bearer U15KDpAy1-TrD7ebm4h8oe5LaCx_f8HdAoQbSmC8'
+
+		},
+  		success :function(response){
+  			console.log(response);
   		},
-  		error:function(response){
-
+  		error:function(reponse){
+  			console.log(response);
   		}
-
 	});
-
 }
+
