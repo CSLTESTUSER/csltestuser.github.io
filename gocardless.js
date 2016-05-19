20dfd29cff9fbcbad27064c3af04a2d0719a93e5
CSLTESTUSER <rajarajan.b@saksoft.co.in>
2016-05-19 20:37:58 +0530

    Update gocardless.js

--- gocardless.js
+++ gocardless.js
@@ -25,10 +25,10 @@ function setupRedirectWorkflow()
   			}
   		},
   		beforeSend:function(xhr){
-			xhr.setRequestHeader("Content-Type","application/json");
-			xhr.setRequestHeader("GoCardless-Version","2015-07-06");
-			xhr.setRequestHeader("Authorization","Bearer U15KDpAy1-TrD7ebm4h8oe5LaCx_f8HdAoQbSmC8");
-			console.log(xhr);
+			//xhr.setRequestHeader("Content-Type","application/json");
+			//xhr.setRequestHeader("GoCardless-Version","2015-07-06");
+			//xhr.setRequestHeader("Authorization","Bearer U15KDpAy1-TrD7ebm4h8oe5LaCx_f8HdAoQbSmC8");
+			//console.log(xhr);
 		}, 
   		success :function(response){
   			alert(response);
