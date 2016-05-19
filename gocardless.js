71e268c70bc6319ec0d907a9eeb06521ffcc84ba
CSLTESTUSER <rajarajan.b@saksoft.co.in>
2016-05-19 20:13:20 +0530

    Update gocardless.js

--- gocardless.js
+++ gocardless.js
@@ -10,10 +10,27 @@ $(function(){
 	});
 });
 
+function handler(response)
+{
+	alert(response);
+}
 
 function setupRedirectWorkflow()
 {
-	$.ajax({ 
+	var invocation = new XMLHttpRequest();
+	var url = 'https://api-sandbox.gocardless.com/redirect_flows';
+	var body = '{redirect_flows: { 'description': 'Security Device Purchase','session_token': 'SESS_somerandomalphanumerictoverify','success_redirect_url': 'http://csltestuser.github.io'}}';
+    	if(invocation)
+    	{
+      		invocation.open('POST', url, true);
+      		invocation.setRequestHeader('Content-Type', 'application/json');
+      		invocation.setRequestHeader('GoCardless-Version', '2015-07-06');
+      		invocation.setRequestHeader('Authorization', 'Bearer U15KDpAy1-TrD7ebm4h8oe5LaCx_f8HdAoQbSmC8');
+      		invocation.onreadystatechange = handler;
+      		invocation.send(body); 
+    	}
+
+	/*$.ajax({ 
 		method:'POST',
 		url:'https://api-sandbox.gocardless.com/redirect_flows',
 		data: {  
@@ -29,6 +46,6 @@ function setupRedirectWorkflow()
   		error:function(reponse){
   			console.log(response);
   		}
-	});
+	});*/
 }
 
